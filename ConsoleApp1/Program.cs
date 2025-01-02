2e743ebe61bc147b089a53465eaee2d19a93c7ec
Matthew Miller <sysnom@gmail.com>
2025-01-01 19:34:52 -0500

    add Plus one

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,7 +7,38 @@
     {
         public static void Main()
         {
-            Console.WriteLine(MajorityElement(new int[] {2, 2, 1, 1, 1, 2, 2}));
+            foreach (var digit in PlusOne(new int[] { 1, 2, 3 }))
+            {
+                Console.WriteLine(digit);
+            }
+        }
+
+        /// <summary>
+        /// You are given a large integer represented as an integer array digits,
+        /// where each digits[i] is the ith digit of the integer. 
+        /// The digits are ordered from most significant to least significant in left-to-right order. 
+        /// The large integer does not contain any leading 0's.
+        /// </summary>
+        /// <param name="digits"></param>
+        /// <returns></returns>
+        public static int[] PlusOne(int[] digits)
+        {
+            if (digits.Length == 0) Array.Empty<int>();
+
+            int n = digits.Length;
+            for (int i = n - 1; i >= 0; i--)
+            {
+                if (digits[i] < 9)
+                {
+                    digits[i]++;
+                    return digits;
+                }
+                digits[i] = 0;
+            }
+            int[] newNumber = new int[n + 1];
+            newNumber[0] = 1;
+
+            return newNumber;
         }
 
         /// <summary>
