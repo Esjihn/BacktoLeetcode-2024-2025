25906af89bd0f163fc16d121b50d54aef7cc1ca1
Matthew Miller <sysnom@gmail.com>
2025-01-06 20:47:33 -0500

    add isHappy square root

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics.Metrics;
+﻿using System.Diagnostics;
+using System;
+using System.Diagnostics.Metrics;
 
 namespace ConsoleApp1
 {
@@ -9,7 +11,35 @@ namespace ConsoleApp1
     {
         public static void Main()
         {
-            Console.WriteLine(IsAnagram("anagram","nagaram"));
+            Console.WriteLine(IsHappy(19));
+        }
+
+        /// <summary>
+        /// Write an algorithm to determine if a number n is happy.
+        /// A happy number is a number defined by the following process:
+        /// Starting with any positive integer, replace the number by the sum of the squares of its digits.
+        /// Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+        /// Those numbers for which this process ends in 1 are happy.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static bool IsHappy(int n)
+        {
+            if (n == 0) return false;
+
+            if (n < 10)
+            {
+                return n == 1 || n == 7;
+            }
+            int sum = 0;
+            while (n > 0)
+            {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+
+            return true;
         }
 
         /// <summary>
