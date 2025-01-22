e8f53c5b00d2c24b73dc1ea9d248d6420800d298
Matthew Miller <sysnom@gmail.com>
2025-01-21 20:48:38 -0500

    add reverse words medium

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -36,7 +36,31 @@ namespace ConsoleApp1
     {
         public static void Main()
         {
-            Console.WriteLine(IntToRoman(8));
+            Console.WriteLine(ReverseWords("  hello world  "));
+        }
+
+        /// <summary>
+        /// Given an input string s, reverse the order of the words.
+        /// A word is defined as a sequence of non-space characters.The words in s will be separated by at least one space.
+        /// Return a string of the words in reverse order concatenated by a single space.
+        /// Note that s may contain leading or trailing spaces or multiple spaces between two words.
+        /// The returned string should only have a single space separating the words. Do not include any extra spaces.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string ReverseWords(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return string.Empty;
+            var reverse = new StringBuilder();
+
+            var words = s.Trim().Split(' ');
+            foreach (var word in words)
+            {
+                if (string.IsNullOrEmpty(word))
+                    continue;
+                reverse = new StringBuilder(word + " " + reverse.ToString());
+            }
+            return reverse.ToString().Trim();
         }
 
         /// <summary>
