490f828037e3b1e589c07edcf6a80233fae73761
Matthew Miller <sysnom@gmail.com>
2025-01-25 20:18:32 -0500

    Add ThreeSum medium

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -39,7 +39,46 @@ namespace ConsoleApp1
     {
         public static void Main()
         {
-            Console.WriteLine(MaxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
+            Console.WriteLine(ThreeSum([-1, 0, 1, 2, -1, -4]));
+        }
+
+        public static IList<IList<int>> ThreeSum(int[] nums)
+        {
+            if (nums.Length == 0) return new List<IList<int>>();
+
+            Array.Sort(nums);
+            IList<IList<int>> result = new List<IList<int>>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i > 0 && nums[i] == nums[i - 1])
+                {
+                    continue; // Skip duplicate values for i
+                }
+                int j = i + 1, k = nums.Length - 1;
+                while (j < k)
+                {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (sum == 0)
+                    {
+                        result.Add(new List<int> { nums[i], nums[j], nums[k] });
+                        j++;
+                        k--;
+                        while (j < k && nums[j] == nums[j - 1]) j++;
+                        while (j < k && nums[k] == nums[k + 1]) k--;
+                    }
+                    else if (sum < 0)
+                    {
+                        j++;
+                    }
+                    else
+                    {
+                        k--;
+                    }
+                }
+            }
+
+            return result;
         }
 
         /// <summary>
