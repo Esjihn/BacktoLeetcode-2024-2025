2c7ae3bd683c140b24308dbc20d94be63c25bd0b
Matthew Miller <sysnom@gmail.com>
2025-02-20 16:00:26 -0500

    add BST Iterator medium update.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -102,6 +102,43 @@ namespace ConsoleApp1
         }
 
         /// <summary>
+        /// Implement the BSTIterator class that represents an iterator over the in-order traversal 
+        /// of a binary search tree (BST): BSTIterator(TreeNode root) Initializes an object of the
+        /// BSTIterator class. The root of the BST is given as part of the constructor.
+        /// The pointer should be initialized to a non-existent number smaller than any element in the BST.
+        /// boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer,
+        /// otherwise returns false. int next() Moves the pointer to the right, then returns the number at the pointer.
+        /// Notice that by initializing the pointer to a non-existent smallest number, the first call to next() 
+        /// will return the smallest element in the BST. You may assume that next() calls will always be valid. 
+        /// That is, there will be at least a next number in the in-order traversal when next() is called.
+        /// </summary>
+        public class BSTIterator
+        {
+            Queue<int> q;
+            public BSTIterator(TreeNode root)
+            {
+                q = new Queue<int>();
+                helper(root, q);
+            }
+            private void helper(TreeNode root, Queue<int> q)
+            {
+                if (root == null) return;
+                helper(root.left, q);
+                q.Enqueue(root.val);
+                helper(root.right, q);
+            }
+            public int Next()
+            {
+                return q.Dequeue();
+            }
+
+            public bool HasNext()
+            {
+                return q.Count != 0;
+            }
+        }
+
+        /// <summary>
         /// You are given the root of a binary tree containing digits from 0 to 9 only.
         /// Each root-to-leaf path in the tree represents a number.
         /// For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
