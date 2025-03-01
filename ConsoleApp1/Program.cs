e6bafa92c435067c1e1f2bfcbd9b89521612c5a0
Matthew Miller <sysnom@gmail.com>
2025-02-28 21:16:54 -0500

    DFS surrounded regions medium

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,6 +16,8 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 using Microsoft.VisualBasic;
 using System.Reflection;
 using System.Numerics;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace ConsoleApp1
 {
@@ -104,6 +106,60 @@ namespace ConsoleApp1
         public static void Main()
         {
             Console.WriteLine(LongestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]));
+        }
+
+        /// <summary>
+        /// You are given an m x n matrix board containing letters 'X' and 'O', capture regions that are surrounded:
+        /// Connect: A cell is connected to adjacent cells horizontally or vertically.
+        /// Region: To form a region connect every 'O' cell.
+        /// Surround: The region is surrounded with 'X' cells if you can connect the region with 'X' cells and none 
+        /// of the region cells are on the edge of the board. To capture a surrounded region, replace all 'O's with 
+        /// 'X's in-place within the original board.You do not need to return anything.
+        /// </summary>
+        /// <param name="board"></param>
+        public void Solve(char[][] board)
+        {
+            int m = board.Length;
+            int n = board[0].Length;
+
+            // Mark all border-connected 'O's as 'T'
+            for (int i = 0; i < m; i++)
+            {
+                if (board[i][0] == 'O') DFS(board, i, 0);
+                if (board[i][n - 1] == 'O') DFS(board, i, n - 1);
+            }
+            for (int j = 0; j < n; j++)
+            {
+                if (board[0][j] == 'O') DFS(board, 0, j);
+                if (board[m - 1][j] == 'O') DFS(board, m - 1, j);
+            }
+
+            // Flip all 'O's to 'X' and 'T's back to 'O'
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    board[i][j] = board[i][j] != 'T' ? 'X' : 'O';
+                }
+            }
+        }
+
+        private void DFS(char[][] board, int i, int j)
+        {
+            int m = board.Length;
+            int n = board[0].Length;
+
+            // Check if the cell is within bounds and is 'O'
+            if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != 'O') return;
+
+            // Mark this cell as 'T'
+            board[i][j] = 'T';
+
+            // Recursively mark all connected 'O's
+            DFS(board, i + 1, j);
+            DFS(board, i - 1, j);
+            DFS(board, i, j + 1);
+            DFS(board, i, j - 1);
         }
 
         /// <summary>
