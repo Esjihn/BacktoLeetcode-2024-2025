c9c4677ddb31d9f3e3b05423742fa042d3f6ea57
Matthew Miller <sysnom@gmail.com>
2025-03-24 21:24:06 -0400

    binary search find min medium

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -25,6 +25,7 @@ using System.Reflection.Emit;
 using System.Reflection.Metadata;
 using System.Xml;
 using System.Net.NetworkInformation;
+using System.Globalization;
 
 namespace ConsoleApp1
 {
@@ -176,6 +177,46 @@ namespace ConsoleApp1
         public static void Main()
         {
             Console.WriteLine(LongestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]));
+        }
+
+        /// <summary>
+        /// Suppose an array of length n sorted in ascending order is rotated between 1 and n times. 
+        /// For example, the array nums = [0,1,2,4,5,6,7] might become: [4, 5, 6, 7, 0, 1, 2] if it was rotated 4 times.
+        /// [0, 1, 2, 4, 5, 6, 7] if it was rotated 7 times. Notice that rotating an array[a[0], a[1], a[2], ..., a[n - 1]] 
+        /// 1 time results in the array[a[n - 1], a[0], a[1], a[2], ..., a[n - 2]]. Given the sorted rotated array nums of
+        /// unique elements, return the minimum element of this array. You must write an algorithm that runs in O(log n) time.
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int FindMin(int[] nums)
+        {
+            // Initialize left and right pointers
+            int left = 0, right = nums.Length - 1;
+
+            // Binary Search
+            while (left < right)
+            {
+                // Calculate mid-point
+                int mid = left + (right - left) / 2;
+
+                // If mid-point element is greater than the last element
+                // of the array, then the minimum element must be in the
+                // right half of the array, so we update left pointer
+                if (nums[mid] > nums[right])
+                {
+                    left = mid + 1;
+                }
+                // Otherwise, the minimum element must be in the left half
+                // of the array, so we update right pointer
+                else
+                {
+                    right = mid;
+                }
+            }
+
+            // At the end of the while loop, left pointer points to the
+            // minimum element of the array, which is the answer
+            return nums[left];
         }
 
         /// <summary>
