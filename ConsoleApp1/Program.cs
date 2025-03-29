ce1d5597a8a2c32cc8cc8fbae670941c70a697ad
Matthew Miller <sysnom@gmail.com>
2025-03-28 20:33:58 -0400

    add num islands graph medium

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -180,6 +180,35 @@ namespace ConsoleApp1
         }
 
         /// <summary>
+        /// Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), 
+        /// return the number of islands. An island is surrounded by water and is formed by connecting 
+        /// adjacent lands horizontally or vertically.You may assume all four edges of the grid are all 
+        /// surrounded by water.
+        /// </summary>
+        /// <param name="grid"></param>
+        /// <returns></returns>
+        public int NumIslands(char[][] grid)
+        {
+            var islands = 0;
+
+            // Traverse the grid one by one
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[0].Length; j++)
+                {
+                    // If grid value is '1', increment the count, and do DFS on the adjacent 4 cells
+                    if (grid[i][j] == '1')
+                    {
+                        DFS(grid, i, j);
+                        islands++;
+                    }
+                }
+            }
+            // Return the island count
+            return islands;
+        }
+
+        /// <summary>
         /// Given the root of a binary tree, determine if it is a valid binary search tree (BST).
         /// A valid BST is defined as follows: The left subtree of a node contains only nodes 
         /// with keys less than the node's key. The right subtree of a node contains only nodes 
