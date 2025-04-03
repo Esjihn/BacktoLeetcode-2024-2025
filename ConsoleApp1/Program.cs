6cfbd5dc2a0505684cc1cf9ecf0d2f1e3dcc30fc
Matthew Miller <sysnom@gmail.com>
2025-04-02 21:59:34 -0400

    pow xn math medium

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -180,6 +180,36 @@ namespace ConsoleApp1
         }
 
         /// <summary>
+        /// Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public double MyPow(double x, int n)
+        {
+            if (n < 0)
+            {
+                x = 1 / x;
+                n = -n;
+            }
+
+            double result = 1;
+            double current_product = x;
+
+            while (n > 0)
+            {
+                if (n % 2 == 1)
+                {
+                    result = result * current_product;
+                }
+                current_product = current_product * current_product;
+                n = n / 2;
+            }
+
+            return result;
+        }
+
+        /// <summary>
         /// Given an integer n, return the number of trailing zeroes in n!.
         /// Note that n! = n* (n - 1) * (n - 2) * ... * 3 * 2 * 1.
         /// </summary>
