27ae9f80facaecd1de7be358513879aedf6d21dd
Matthew Miller <sysnom@gmail.com>
2025-04-09 20:43:18 -0400

    MD DP unique paths 2 medium

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -180,6 +180,44 @@ namespace ConsoleApp1
         }
 
         /// <summary>
+        /// You are given an m x n integer array grid. There is a robot initially located
+        /// at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right
+        /// corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any
+        /// point in time. An obstacle and space are marked as 1 or 0 respectively in grid.
+        /// A path that the robot takes cannot include any square that is an obstacle. Return the 
+        /// number of possible unique paths that the robot can take to reach the bottom-right corner.
+        /// The testcases are generated so that the answer will be less than or equal to 2 * 109.
+        /// </summary>
+        /// <param name="obstacleGrid"></param>
+        /// <returns></returns>
+        public int UniquePathsWithObstacles(int[][] obstacleGrid)
+        {
+            if (obstacleGrid == null || obstacleGrid.Length == 0 || obstacleGrid[0].Length == 0 || obstacleGrid[0][0] == 1)
+            {
+                return 0;
+            }
+
+            int m = obstacleGrid.Length;
+            int n = obstacleGrid[0].Length;
+
+            int[] previous = new int[n];
+            int[] current = new int[n];
+            previous[0] = 1;
+
+            for (int i = 0; i < m; i++)
+            {
+                current[0] = obstacleGrid[i][0] == 1 ? 0 : previous[0];
+                for (int j = 1; j < n; j++)
+                {
+                    current[j] = obstacleGrid[i][j] == 1 ? 0 : current[j - 1] + previous[j];
+                }
+                Array.Copy(current, previous, n);
+            }
+
+            return previous[n - 1];
+        }
+
+        /// <summary>
         /// Given a m x n grid filled with non-negative numbers, find a path from top
         /// left to bottom right, which minimizes the sum of all numbers along its path.
         /// Note: You can only move either down or right at any point in time.
