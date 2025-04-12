377f55687ee6862ffa2b586a68d4d38d475b5753
Matthew Miller <sysnom@gmail.com>
2025-04-11 20:29:51 -0400

    isInterleave MD DP medium

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -180,6 +180,44 @@ namespace ConsoleApp1
         }
 
         /// <summary>
+        /// Given strings s1, s2, and s3, find whether s3 is formed by an interleaving of s1 and s2.
+        /// An interleaving of two strings s and t is a configuration where s and t are divided into n
+        /// and m substrings respectively, such that:s = s1 + s2 + ... + sn
+        /// t = t1 + t2 + ... + tm
+        /// |n - m| <= 1
+        /// The interleaving is s1 + t1 + s2 + t2 + s3 + t3 + ... or t1 + s1 + t2 + s2 + t3 + s3 + ...
+        /// Note: a + b is the concatenation of strings a and b.
+        /// </summary>
+        /// <param name="s1"></param>
+        /// <param name="s2"></param>
+        /// <param name="s3"></param>
+        /// <returns></returns>
+        public bool IsInterleave(string s1, string s2, string s3)
+        {
+            int m = s1.Length, n = s2.Length, l = s3.Length;
+            if (m + n != l) return false;
+
+            bool[] dp = new bool[n + 1];
+            dp[0] = true;
+
+            for (int j = 1; j <= n; ++j)
+            {
+                dp[j] = dp[j - 1] && s2[j - 1] == s3[j - 1];
+            }
+
+            for (int i = 1; i <= m; ++i)
+            {
+                dp[0] = dp[0] && s1[i - 1] == s3[i - 1];
+                for (int j = 1; j <= n; ++j)
+                {
+                    dp[j] = (dp[j] && s1[i - 1] == s3[i + j - 1]) || (dp[j - 1] && s2[j - 1] == s3[i + j - 1]);
+                }
+            }
+
+            return dp[n];
+        }
+
+        /// <summary>
         /// Given a string s, return the longest palindromic substring in s.
         /// </summary>
         /// <param name="s"></param>
